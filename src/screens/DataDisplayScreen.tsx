8bc62b591300ccb750387af3231c883175d1633d
vap1 <vaibhavvasant1705@gmail.com>
2023-08-27 22:16:37 +0530

    First Commit

--- src/screens/DataDisplayScreen.tsx
+++ src/screens/DataDisplayScreen.tsx
@@ -1,43 +1,37 @@
 
 import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList } from 'react-native';
-import { User } from '../types/Type';
-import { getDataDisplay } from '../apis/DataDisplayApi';
-import UserDetailsList from '../components/UserDetailsList';
-import SearchBar from '../components/SearchBar';
+import { View, Text } from 'react-native';
+import { User, DataDisplayResponse } from '../types/Type';
+import DataDisplayApi from '../apis/DataDisplayApi';
 
 const DataDisplayScreen: React.FC = () => {
   const [users, setUsers] = useState<User[]>([]);
-  const [searchResults, setSearchResults] = useState<User[]>([]);
 
   useEffect(() => {
-    fetchData();
+    fetchDataDisplay();
   }, []);
 
-  const fetchData = async () => {
+  const fetchDataDisplay = async () => {
     try {
-      const response = await getDataDisplay();
+      const response: DataDisplayResponse = await DataDisplayApi.fetchDataDisplay();
       setUsers(response.users);
     } catch (error) {
-      console.error('Error fetching data:', error);
+      // Handle error
     }
-  };
-
-  const handleSearch = (keyword: string) => {
-    const results = users.filter((user) =>
-      user.firstName.toLowerCase().includes(keyword.toLowerCase())
-    );
-    setSearchResults(results);
   };
 
   return (
     <View>
-      <SearchBar onSearch={handleSearch} />
-      {searchResults.length > 0 ? (
-        <UserDetailsList users={searchResults} />
-      ) : (
-        <UserDetailsList users={users} />
-      )}
+      {users.map((user, index) => (
+        <View key={index}>
+          <Text>{user.userId}</Text>
+          <Text>{user.firstName}</Text>
+          <Text>{user.lastName}</Text>
+          <Text>{user.email}</Text>
+          <Text>{user.phone}</Text>
+          <Text>{user.address}</Text>
+        </View>
+      ))}
     </View>
   );
 };
