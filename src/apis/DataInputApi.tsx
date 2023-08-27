80353845bb290631fadf3a79cb39fa624873f2c9
vap1 <vaibhavvasant1705@gmail.com>
2023-08-27 22:16:33 +0530

    First Commit

---
+++ src/apis/DataInputApi.tsx
@@ -0,0 +1,18 @@
+
+import axios, { AxiosResponse } from 'axios';
+import { DataInputRequest, DataInputResponse } from '../types/Type';
+
+const BASE_URL = 'YOUR_BACKEND_API_BASE_URL';
+
+const DataInputApi = {
+  postDataInput: async (dataInputRequest: DataInputRequest): Promise<DataInputResponse> => {
+    try {
+      const response: AxiosResponse<DataInputResponse> = await axios.post(`${BASE_URL}/data-input`, dataInputRequest);
+      return response.data;
+    } catch (error) {
+      throw new Error('Failed to submit data input');
+    }
+  },
+};
+
+export default DataInputApi;
